d48f0075629ba4e5624773b09a950709665212f4
KJ Witherspoon <50391760+Spooncodes@users.noreply.github.com>
2020-08-04 00:37:45 -0400

    hashmap and stack

--- src/main/java/io/codedifferently/OpenCloseChecker.java
+++ src/main/java/io/codedifferently/OpenCloseChecker.java
@@ -1,4 +1,35 @@
 package io.codedifferently;
 
+
+import java.util.HashMap;
+import java.util.Stack;
+
 public class OpenCloseChecker {
+
+    public static boolean openCloseChecker(String input){
+
+        HashMap<Character, Character> openClose = new HashMap<Character, Character>(); // key, value
+        Stack<Character> parentheses = new Stack<Character>();
+        openClose.put('(', ')'); //matching keys with they values
+        openClose.put('[', ']');
+        openClose.put('{', '}');
+        openClose.put('<', '>');
+        openClose.put('"', '"');
+        openClose.put('\'','\'');
+
+        char[] characters = input.toCharArray(); //change string input to char array
+        for(Character element : characters) { // for each element in char array
+            if (openClose.containsKey(element)) { // push key to top of stack
+                parentheses.push(element);
+            } else if (openClose.containsValue(element)) { // look at the values
+                if (!parentheses.empty() && openClose.get(parentheses.peek()) == element) {
+                    parentheses.pop(); // remove element from stack
+                } else {
+                    return false;
+                }
+            }
+        }
+        return parentheses.empty();
+
+    }
 }
--- src/test/java/io/codedifferently/OpenCloseCheckerTest.java
+++ src/test/java/io/codedifferently/OpenCloseCheckerTest.java
@@ -1,5 +1,70 @@
 package io.codedifferently;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class OpenCloseCheckerTest {
+
+    OpenCloseChecker openCloseChecker;
+
+    @Before
+    public void setup(){
+        this.openCloseChecker = new OpenCloseChecker();
+    }
+
+    @Test
+    public void parenthesisTest(){
+        String input = "a(b)c(d)e(f)g";
+
+        Boolean actual = openCloseChecker.openCloseChecker(input);
+
+        Assert.assertTrue(input, actual);
+    }
+
+    @Test
+    public void parenthesisTest2(){
+        String input = ")";
+
+        Boolean actual = false;
+
+        Assert.assertFalse(input, actual);
+    }
+
+    @Test
+    public void parenthesisTest3(){
+        String input = "a(bcd(ef))";
+
+        Boolean actual = openCloseChecker.openCloseChecker(input);
+
+        Assert.assertTrue(input, actual);
+    }
+
+    @Test
+    public void parenthesisTest4(){
+        String input = "a(bc(d)))";
+
+        Boolean actual = openCloseChecker.openCloseChecker(input);
+
+        Assert.assertFalse(input, actual);
+    }
+
+    @Test
+    public void squareBracketTest(){
+        String input = "[]";
+
+        Boolean actual = openCloseChecker.openCloseChecker(input);
+
+        Assert.assertTrue(input, actual);
+    }
+
+    @Test
+    public void squareBracketTest2(){
+        String input = "]";
+
+        Boolean actual = openCloseChecker.openCloseChecker(input);
+
+        Assert.assertFalse(input, actual);
+    }
 
 }
